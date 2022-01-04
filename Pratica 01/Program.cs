003a7fae42a0538ce98cb461558e07fa50df62e4
hudson-araujo <hudsonab@live.com>
2022-01-04 15:52:03 -0300

    Adicionar arquivos de projeto.

---
+++ Pratica 01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pratica 
+{
+
+    class Program
+    {
+             static void Main(string[] args)
+             {
+        var a = "Olá, ";
+        var b = "Mundo! SAI ZIKA!";
+        Console.WriteLine(a + b);
+              }
+    }
+}
